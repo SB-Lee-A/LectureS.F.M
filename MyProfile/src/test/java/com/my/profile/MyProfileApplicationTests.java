4cfaaed832fae16e6272e8a21e44def198bef2ed
SB-Lee-A <teemocscsg@naver.com>
2020-11-25 16:17:56 +0900

    프로필

---
+++ MyProfile/src/test/java/com/my/profile/MyProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
